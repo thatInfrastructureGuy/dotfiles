6f8ea7d176361b073ba8262031a64a9b0e7edfb1
thatInfrastructureGuy <thatInfrastructureGuy@gmail.com>
2025-02-15 17:07:07 -0800

    Add run_once script
    Signed-off-by: thatInfrastructureGuy <thatInfrastructureGuy@gmail.com>

---
+++ run_once_startup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+#/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm && ~/.tmux/plugins/tpm/bin/install_plugins
