6bfec97a69774f01abb97daeeac78b6dce203fed
thatInfrastructureGuy <thatInfrastructureGuy@gmail.com>
2025-03-10 22:39:21 -0700

    Remove old wiki
    Signed-off-by: thatInfrastructureGuy <thatInfrastructureGuy@gmail.com>

--- private_dot_config/nvim_tig/lua/plugins/vimwiki.lua
+++ private_dot_config/nvim_tig/lua/plugins/vimwiki.lua
@@ -1,12 +1,15 @@
 return {
     {
         'vimwiki/vimwiki',
+        enabled = false,
     },
     {
         'michal-h21/vimwiki-sync',
+        enabled = false,
     },
     {
         'michal-h21/vim-zettel',
+        enabled = false,
         config = function()
             vim.cmd [[
                 " Markdown - Disable folds
@@ -51,6 +54,33 @@ return {
 
                 let g:sync_taskwarrior = 0
            ]]
+        end
+    },
+    {
+        "zk-org/zk-nvim",
+        config = function()
+            require("zk").setup({
+                -- Can be "telescope", "fzf", "fzf_lua", "minipick", "snacks_picker",
+                -- or select" (`vim.ui.select`). It's recommended to use "telescope",
+                -- "fzf", "fzf_lua", "minipick", or "snacks_picker".
+                picker = "fzf_lua",
+
+                lsp = {
+                    -- `config` is passed to `vim.lsp.start_client(config)`
+                    config = {
+                        cmd = { "zk", "lsp" },
+                        name = "zk",
+                        -- on_attach = ...
+                        -- etc, see `:h vim.lsp.start_client()`
+                    },
+
+                    -- automatically attach buffers in a zk notebook that match the given filetypes
+                    auto_attach = {
+                        enabled = true,
+                        filetypes = { "markdown" },
+                    },
+                },
+            })
         end
     },
 }
